ac623f9a38cec1eaa8ce4c2c9f50ee980ecd6a7c
Ravitejareddy Kona <Ravitejareddy.Kona@derivco.co.uk>
2022-04-28 21:07:55 +0100

    some tags were added

--- c4-ec2-instance.tf
+++ c4-ec2-instance.tf
@@ -8,6 +8,8 @@ resource "aws_instance" "my-ec2-vm" {
   vpc_security_group_ids = [aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id]
   tags = {
     "Name" = "Terraform-Cloud-${count.index}"
+    "Tier" = "Front end"
+    "Author" = "Raviteja"
   }
 }
 
